0c20ee6e66bc92d97874b432a0979a369bdb2b9d
david43221 <131267299+david43221@users.noreply.github.com>
2025-05-21 10:56:12 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') # TEST
+    return render_template('hello.html') # TEST2
 
 key = Fernet.generate_key()
 f = Fernet(key)
